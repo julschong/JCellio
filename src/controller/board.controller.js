c656346a120bd487b1c2f33ead6b73efb5d082a7
Julius Lee <7julius.lee@gmail.com>
2021-05-30 02:20:11 -0700

    added error response for getOneBoard

--- src/controller/board.controller.js
+++ src/controller/board.controller.js
@@ -1,6 +1,7 @@
 const prisma = require('../db/db');
 const { board, column } = prisma;
 const asyncHandler = require('express-async-handler');
+const ErrorResponse = require('../helper/errorResponse');
 
 exports.getAllBoards = asyncHandler(async (req, res, next) => {
     console.log(req.info);
@@ -13,7 +14,6 @@ exports.getAllBoards = asyncHandler(async (req, res, next) => {
 
 exports.createNewBoard = asyncHandler(async (req, res, next) => {
     const newBoard = await board.create({ data: { userId: req.info.id } });
-    console.log(newBoard);
     res.status(201).json({ success: true, data: newBoard });
 });
 
@@ -23,12 +23,21 @@ exports.updateBoard = asyncHandler(async (req, res, next) => {
 
 exports.deleteBoard = asyncHandler(async (req, res, next) => {
     const id = req.params.id;
-    console.log(typeof id);
     await board.delete({ where: { id } });
 
     res.status(200).json({ success: true, data: [] });
 });
 
 exports.getOneBoard = asyncHandler(async (req, res, next) => {
-    res.send('get one board');
+    const id = req.params.id;
+    const foundBoard = await board.findUnique({
+        where: { id },
+        include: { Column: true }
+    });
+
+    if (!foundBoard) {
+        throw new ErrorResponse(400, 'Board cannot be found');
+    }
+
+    res.status(200).json({ success: true, data: foundBoard });
 });
