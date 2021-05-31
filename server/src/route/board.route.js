169a3cdadad98ec64d88f8955c2ffe1204f1791d
Julius Lee <7julius.lee@gmail.com>
2021-05-31 00:27:47 -0700

    added nested get all boards

--- server/src/route/board.route.js
+++ server/src/route/board.route.js
@@ -9,10 +9,12 @@ const {
 const { authorization } = require('../middleware/authorization');
 const { idReformat } = require('../middleware/idReformat');
 const columnRoute = require('./column.route');
+const taskRoute = require('./task.route');
 
 const boardRoute = express.Router();
 
 boardRoute.use('/:boardId/columns', columnRoute);
+boardRoute.use('/:boardId/columns/:columnId/tasks', taskRoute);
 boardRoute.use(authorization);
 
 boardRoute.route('/').get(getAllBoards).post(createNewBoard);
