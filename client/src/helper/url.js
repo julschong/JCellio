cdfa5cea764d7b64deb04c763a72e47d85312f49
Julius <71372051+julschong@users.noreply.github.com>
2021-06-12 01:04:55 -0700

    added comments and task details in prisma

--- client/src/helper/url.js
+++ client/src/helper/url.js
@@ -1,3 +1,3 @@
 export const FETCH = Object.freeze({
-    BASE_URL: 'https://todo-backend-julschong.herokuapp.com/api/v1'
+    BASE_URL: 'https://localhost:3003/api/v1' //https:todo-backend-julschong.herokuapp.com/api/v1'
 });
--- server/prisma/migrations/20210530085936_change_relations_to_optionalagain/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Column" ALTER COLUMN "boardId" DROP NOT NULL;
-
--- AlterTable
-ALTER TABLE "Task" ALTER COLUMN "columnId" DROP NOT NULL;
