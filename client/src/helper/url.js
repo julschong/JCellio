7095ca20d6dfda18e06d1673a58a0d33d4a7a0cd
Julius Lee <7julius.lee@gmail.com>
2021-06-13 00:20:54 -0700

    fixedURL

--- client/src/helper/url.js
+++ client/src/helper/url.js
@@ -2,5 +2,5 @@ export const FETCH = Object.freeze({
     BASE_URL:
         process.env.NODE_ENV === 'development'
             ? 'http://localhost:3003/api/v1'
-            : 'https:todo-backend-julschong.herokuapp.com/api/v1'
+            : 'https://todo-backend-julschong.herokuapp.com/api/v1'
 });
