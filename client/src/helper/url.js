54a88c00e3e5a88c8ed67df085b947a83d239e40
Julius Lee <71372051+julschong@users.noreply.github.com>
2021-08-27 17:01:26 -0700

    added to use env variable

--- client/src/helper/url.js
+++ client/src/helper/url.js
@@ -1,6 +1,6 @@
 export const FETCH = Object.freeze({
     BASE_URL:
         process.env.NODE_ENV === 'development'
-            ? 'http://localhost:3003/api/v1'
-            : 'https://todo-backend-julschong.herokuapp.com/api/v1'
+            ? process.env.REACT_APP_LOCAL_BASE_URL
+            : process.env.REACT_APP_PROD_BASE_URL
 });
